c152f10fd853dec2ffcbbb2c2562c2db6e814848
ShakirMahmud <shakirmahmud50@gmail.com>
2025-01-14 19:11:11 +0600

    jwt create token

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,18 @@
 const { getDatabase } = require('../config/dbConnection');
 const usersCollection = getDatabase().collection('users');
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+
+const createToken = async (req, res) => {
+    try {
+        const user = req.body;
+        const token = jwt.sign(user , process.env.JWT_SECRET, { expiresIn:process.env.JWT_EXPIRES_IN });
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: "Error creating token", error });
+    }
+}
 
 const getUsers = async (req, res) => {
   try {
